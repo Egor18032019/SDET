0e6199761eeb97082027bf7eee65df677811d138
Egor <goro4@mail.ru>
2023-04-02 09:39:14 +0500

    вынес поиск и создание раздельно

--- src/test/java/utils/Helpers.java
+++ src/test/java/utils/Helpers.java
@@ -13,6 +13,9 @@ import java.util.List;
 public class Helpers {
     @Step("Создание нового клиента +  accept модального окна")
     public static void creatingCustomer(String name, String last, String postalCode, MainPage page, WebDriver driver, WebDriverWait wait) {
+        Waiters.waitVisibilityElement(page.firstNameInput, wait);
+        Waiters.waitVisibilityElement(page.lastNameInput, wait);
+        Waiters.waitVisibilityElement(page.postalCodeInput, wait);
         page.creatNewCustomer(name, last, postalCode);
         Waiters.WaitingModalWindow(wait);
         Alert alert = driver.switchTo().alert();
@@ -22,20 +25,24 @@ public class Helpers {
         alert.accept();
     }
 
+    @Step("Поиск клиентов содержащих в ФИО {searchString}")
     public static List<WebElement> searchCustomer(String searchString, String name, String last, String postalCode, MainPage mainPage, WebDriver driver, WebDriverWait wait) {
         creatingCustomer(name, last, postalCode, mainPage, driver, wait);
+        mainPage.clickButtonCustomer();
+        Waiters.waitVisibilityElement(mainPage.searchCustomerInput, wait);
         mainPage.searchCustomer(searchString);
         List<WebElement> table = mainPage.rowsFromTableCustomer;
         return table;
     }
 
+    @Step("Проверка все ли элементы списка содержат {searchString}")
     public static boolean isContainsSearchString(String searchString, String name, String lastName, String postalCode, MainPage mainPage, WebDriver driver, WebDriverWait wait) {
         List<WebElement> table = searchCustomer(searchString, name, lastName, postalCode, mainPage, driver, wait);
         //если лист пустой, то тест не пройдет
         boolean cellHaveFirstName = false;
         for (WebElement element : table) {
-            String row = element.getText().split(" ")[0];
-            cellHaveFirstName = row.contains(Const.firstName);
+            String row = element.getText();
+            cellHaveFirstName = row.contains(searchString);
             if (!cellHaveFirstName) break;
         }
         return cellHaveFirstName;
